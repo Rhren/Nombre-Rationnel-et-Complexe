c0d65ea01f84f0fe439712381cc60d725d96d2a6
RAHARINIRINA Nancia31 <167851356+Rhren@users.noreply.github.com>
2025-02-06 18:10:13 +0300

    Update Rationnel.h

--- Rationnel.h
+++ Rationnel.h
@@ -9,11 +9,14 @@ class Rationnel
         int num , deno;
     public:
         Rationnel();
+        ~Rationnel();
         void setRatio();
         void afficher(std::string op,Rationnel n2,Rationnel r);
         Rationnel addition(Rationnel n2);
         Rationnel soustraction(Rationnel n2);
         Rationnel division(Rationnel n2);
         Rationnel multiplication(Rationnel n2);
+        int getNum();
+        int getDeno();
 };
 #endif
