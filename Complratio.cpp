b04e1356e989f16eba1e6448153400112308bba0
RAHARINIRINA Nancia31 <167851356+Rhren@users.noreply.github.com>
2025-02-06 18:15:28 +0300

    Update Complratio.cpp

--- Complratio.cpp
+++ Complratio.cpp
@@ -8,7 +8,7 @@ int main()
     Complexerationnel X,Y,S;
     cout << "=> 1er nombre complexe:" << endl;
     X.setValeur();
-    cout << "=> 1er nombre complexe:" << endl;
+    cout << "=> 2eme nombre complexe:" << endl;
     Y.setValeur();
     S = X.addition(Y);
     X.afficher("+",Y,S);
