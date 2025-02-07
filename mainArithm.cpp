704c54a23ef8b57dd9b035abb4775b0842013659
RAHARINIRINA Nancia31 <167851356+Rhren@users.noreply.github.com>
2025-02-07 11:40:01 +0300

    Update mainArithm.cpp

--- mainArithm.cpp
+++ mainArithm.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     Rationnel A2,S;
-    Complexerationnel C,R;
+    Complexerationnel C,R,CR;
     int a;
     cout << "Entrer un entier: ";
     cin >> a;
@@ -17,8 +17,12 @@ int main()
     //additionner d'un type int et type Rationnel
     S = Arithmetique::addition(a,A2);
     Arithmetique::affichage(a,A2,S);
+    //additionner d'un type int et type Complexerationnel
     R = Arithmetique::addition(a,C);
     Arithmetique::affichage(a,C,R);
-    
+    //additionner d'un type Rationnel et type Complexerationnel
+    CR = Arithmetique::addition(A2,C);
+    Arithmetique::affichage(A2,C,CR);
+
     return 0;
 }
