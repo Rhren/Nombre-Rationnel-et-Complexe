d85b95a8f1d372d52246723c382dd1fead18597d
RAHARINIRINA Nancia31 <167851356+Rhren@users.noreply.github.com>
2025-02-07 11:37:09 +0300

    Update Complexerationnel.h

--- Complexerationnel.h
+++ Complexerationnel.h
@@ -1,3 +1,6 @@
+#ifndef COMPLEXERATIONNEL_H
+#define COMPLEXERATIONNEL_H
+
 #include "Rationnel.h"
 #include <string>
 class Complexerationnel
@@ -7,7 +10,13 @@ class Complexerationnel
     public:
         Complexerationnel();
         ~Complexerationnel();
+        Rationnel getReel();
+        Rationnel getIm();
+        void setIm(int n,int d);
+        void setReel(int n,int d);
         Complexerationnel addition(Complexerationnel &C2);
         void afficher(std::string op,Complexerationnel &C2,Complexerationnel &S);
         void setValeur();
 };
+
+#endif
