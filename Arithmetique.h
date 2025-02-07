f682367396505b2d7b9c2b49a30635974e3b9da3
RAHARINIRINA Nancia31 <167851356+Rhren@users.noreply.github.com>
2025-02-07 11:39:17 +0300

    Update Arithmetique.h

--- Arithmetique.h
+++ Arithmetique.h
@@ -4,10 +4,16 @@
 class Arithmetique
 {
     public:
+        //additionner d'un type int et type Rationnel
         static Rationnel addition(int a,Rationnel &R);
         static void affichage(int a,Rationnel &R,Rationnel &S);
+        //additionner d'un type int et type Complexerationnel
         static Complexerationnel addition(int a,Complexerationnel &C);
         static void affichage(int a,Complexerationnel &C,Complexerationnel &R);
+        //additionner d'un type Rationnel et type Complexerationnel
+        static Complexerationnel addition(Rationnel &R,Complexerationnel &C);
+        static void affichage(Rationnel &R,Complexerationnel &C,Complexerationnel &CR);
+
        /* static double addition(int a,double d);
         static Complexerationnel addition(Rationnel &R,Complexerationnel &C);*/
 };
